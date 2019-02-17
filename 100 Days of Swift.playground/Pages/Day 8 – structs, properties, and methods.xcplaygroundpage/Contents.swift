c150bb251e987f0ce12c795953320b3b166615f6
Brian Phillips <unipheas@protonmail.com>
2019-02-17 18:35:34 +0800

    Completed day 8

---
+++ 100 Days of Swift.playground/Pages/Day 8 – structs, properties, and methods.xcplaygroundpage/Contents.swift
@@ -0,0 +1,112 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: Creating your own struct
+
+struct Sport {
+	var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+
+//: Computed properties
+
+struct Sport1 {
+	var name: String
+	var isOlympicSport: Bool
+	
+	var olympicStatus: String {
+		if isOlympicSport {
+			return "\(name) is an Olympic sport"
+		} else {
+			return "\(name) is not an Olympic sport"
+		}
+	}
+}
+
+let chessBoxing = Sport1(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+//: Property observers
+
+struct Progress {
+	var task: String
+	var amount: Int
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct Progress1 {
+	var task: String
+	var amount: Int {
+		didSet {
+			print("\(task) is now \(amount)% complete")
+		}
+	}
+}
+
+var progress1 = Progress1(task: "Loading data", amount: 0)
+progress1.amount = 30
+progress1.amount = 80
+progress1.amount = 100
+
+//: Methods
+
+struct City {
+	var population: Int
+	
+	func collectTaxes() -> Int {
+		return population * 1000
+	}
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+//: Mutating methods
+
+struct Person {
+	var name: String
+	
+	mutating func makeAnonymous() {
+		name = "Anonymous"
+	}
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+//: Properties and methods of strings
+
+let string = "Do or do not, there is no try"
+
+print(string.count)
+
+print(string.hasPrefix("Do"))
+
+print(string.uppercased())
+
+print(string.sorted())
+
+//: Properties and methods of arrays
+
+var toys = ["Woody"]
+
+print(toys.count)
+
+toys.append("Buzz")
+
+toys.index(of: "Buzz")
+
+print(toys.sorted())
+
+toys.remove(at: 0)
+
+//: [Next](@next)
--- 100 Days of Swift.playground/Pages/Index.xcplaygroundpage/Contents.swift
+++ 100 Days of Swift.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -11,6 +11,10 @@
 * Day 10 – classes and inheritance
 * Day 11 – protocols, extensions, and protocol extensions
 * Day 12 – optionals, unwrapping, and typecasting
+* Day 13 – Swift review, day one
+* Day 14 – Swift review, day two
+* Day 15 – Swift review, day three
+* Day 16 – Project 1, part one
 */
 
 //: [Next](@next)
