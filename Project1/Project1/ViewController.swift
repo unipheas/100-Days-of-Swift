42ed8f55a78dc5572e74c376881e45326b7d2b90
Brian Phillips <unipheas@protonmail.com>
2019-02-19 17:59:18 +0800

    Completed day 17

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		title = "Storm Viewer"
+		navigationController?.navigationBar.prefersLargeTitles = true
+
 		let fileManager = FileManager.default
 		let path = Bundle.main.resourcePath!
 		let items = try? fileManager.contentsOfDirectory(atPath: path)
@@ -22,15 +25,23 @@ class ViewController: UITableViewController {
 				pictures.append(item)
 			}
 		}
-		print(pictures)
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return pictures.count
 	}
+	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
 		cell.textLabel?.text = pictures[indexPath.row]
 		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let viewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+			viewController.selectedImage = pictures[indexPath.row]
+			
+			navigationController?.pushViewController(viewController, animated: true)
+		}
 	}
 }
