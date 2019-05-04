063f3249796dd0a309eb6610b7aaa8330a266467
Brian Phillips <unipheas@protonmail.com>
2019-05-04 11:19:32 +0800

    Completed day 92

---
+++ Milestone_projects_25-27/Milestone_projects_25-27/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Milestone_projects_25-27
+//
+//  Created by Brian Phillips on 5/2/19.
+//  Copyright © 2019 Titanian Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var tabBar: UITabBar!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+}
+
